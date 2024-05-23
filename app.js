93eb921a32bae0cbaa1c1285b8c6215f28590733
MartinSal1 <martysalwork@gmail.com>
2024-05-23 13:38:43 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('You see, i got my degree and then i was like...'));
+app.get('/', (req, res) => res.send('You see, i got my degree and then i was like lets just go on this path that is unknown you know?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
